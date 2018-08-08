943b5959f3a5a29c98615432cc86ca77a485a63a
Oscar Martinez Sanchez <omsz90@live.com.mx>
2018-08-07 23:09:03 -0500

    :tada: Initial commit
    Files Uploaded

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# CountDownTimer
-CountDown Timer Test
+# countdown
+
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn run serve
+```
+
+### Compiles and minifies for production
+```
+yarn run build
+```
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <h2>Initial App</h2>
+  </div>
+</template>
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
